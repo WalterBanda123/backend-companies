c7bc680c8d90f2834a63a8f93988e5b1154e9a64
Walter Banda <walterbanda98@gmail.com>
2023-05-10 11:10:15 +0200

    commit

--- form/routes/users.js
+++ form/routes/users.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const User = require("./../models/user");
 const router = express.Router();
 const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken');
-
+const jwt = require("jsonwebtoken");
 
 //-REGISTERING A NEW USER----
 router.post("/signup", async (req, res) => {
@@ -44,80 +43,78 @@ router.post("/signup", async (req, res) => {
   }
 });
 
-
 //---LOGGING A USER---
 router.post("/login", async (req, res) => {
-    const user = await User.find({ email: req.body.email });
-  
-    if (user.length < 1) {
+  const user = await User.find({ email: req.body.email });
+
+  if (user.length < 1) {
+    return res.status(401).json({
+      error: "Auth failed",
+    });
+  }
+  bcrypt.compare(req.body.password, user[0].password, async (err, result) => {
+    if (err) {
       return res.status(401).json({
-        error: "Auth failed",
+        error:err,
+        errorMessage: "Auth failed",
       });
     }
-  
-    bcrypt.compare(req.body.password, user[0].password, async (err, result) => {
-      if (err) {
-        return res.status(401).json({
-          error: "Auth failed",
-        });
-      }
-  
-      if (result) {
-        const token = jwt.sign(
-          {
-            email: user[0].email,
-            userId: user[0]._id,
-          },
-          process.env.ENV_SECRET_KEY,
-          {
-            expiresIn: "1h",
-          }
-        );
-        return res.status(200).json({
-           message: "Auth was successful",
-          _id: user[0]._id,
-          token: token,
-        });
-      }
-  
-      res.status(401).json({
-        error: "Auth failed",
+
+    if (result) {
+      const token = jwt.sign(
+        {
+          email: user[0].email,
+          userId: user[0]._id,
+        },
+        process.env.ENV_SECRET_KEY,
+        {
+          expiresIn: "1h",
+        }
+      );
+      return res.status(200).json({
+        message: "Auth was successful",
+        _id: user[0]._id,
+        token: token,
       });
+    }
+    res.status(401).json({
+      errorMessage: "Auth failed",
     });
   });
+});
 
-  //----GETTING USER BY ID -----
+//----GETTING USER BY ID -----
 
 router.get("/:userID", async (req, res) => {
-    try {
-      const userId = req.params.userID;
-      const user = await User.findById(userId);
-  
-      res.status(201).json({
-        message: "Obtained the user",
-        user: user,
-      });
-    } catch (error) {
-      res.status(500).json({
-        error: error,
-      });
-    }
-  });
-  
-  //---GETTING ALL USERS ----
-  router.get("/", async (req, res) => {
-    try {
-      // User.insertMany(users);
-      const userList = await User.find();
-      res.status(201).json({
-        allUsers: userList,
-      });
-    } catch (error) {
-      res.status(500).json({
-        error: error,
-        text: "failed to retrieve users",
-      });
-    }
-  });
+  try {
+    const userId = req.params.userID;
+    const user = await User.findById(userId);
 
-  module.exports = router
+    res.status(201).json({
+      message: "Obtained the user",
+      user: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error,
+    });
+  }
+});
+
+//---GETTING ALL USERS ----
+router.get("/", async (req, res) => {
+  try {
+    // User.insertMany(users);
+    const userList = await User.find();
+    res.status(201).json({
+      allUsers: userList,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error,
+      text: "failed to retrieve users",
+    });
+  }
+});
+
+module.exports = router;
