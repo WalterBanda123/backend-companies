b145de68543b18400d34102b9b09126cdf72333f
Walter Banda <walterbanda98@gmail.com>
2023-05-11 16:06:22 +0200

    commit

--- app.js
+++ app.js
@@ -8,16 +8,25 @@ const userRoutes = require("./form/routes/users");
 
 mongoose.connect("mongodb://127.0.0.1:27017/SalesDrive");
 
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
 const app = express();
+app.use(cors(corsOptions)); // Use this after the variable declaration
+// app.use(cors());
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
-app.use(cors());
 
 mongoose.Promise = global.Promise;
 
 app.use("/companies", companyRoutes);
 app.use("/users", userRoutes);
+
 app.use((req, res) => {
   const error = new Error();
 
